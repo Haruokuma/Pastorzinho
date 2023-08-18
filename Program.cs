6700163410291885fc4e3ac1b5ed6ea473735415
Haruokuma <e107ti@etec.sp.gov.br>
2023-08-18 20:42:29 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿Console.Beep(1320, 200);
+Console.Write("Dó, ");
+Console.Beep(1485, 200);
+Console.Write("Ré, ");
+Console.Beep(1650, 200);
+Console.Write("Mi, ");
+Console.Beep(1759, 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Console.Write("Fá, ");
+Thread.Sleep(500);
+
+Console.WriteLine("");
+
+Console.Beep(1320, 200);
+Console.Write("Dó, ");
+Console.Beep(1485, 200);
+Console.Write("Ré, ");
+Console.Beep(1320, 200);
+Console.Write("Dó, ");
+Console.Beep(1485, 200);
+Console.Write("Ré, ");
+Console.Beep(1485, 200);
+Console.Write("Ré, ");
+Console.Beep(1485, 200);
+Console.Write("Ré, ");
+Thread.Sleep(500);
+
+Console.WriteLine("");
+
+Console.Beep(1320, 200);
+Console.Write("Dó, ");
+Console.Beep(1980, 200);
+Console.Write("Sol, ");
+Console.Beep(1759, 200);
+Console.Write("Fá, ");
+Console.Beep(1650, 200);
+Console.Write("Mi, ");
+Console.Beep(1650, 200);
+Console.Write("Mi, ");
+Console.Beep(1650, 200);
+Console.Write("Mi, ");
+Thread.Sleep(500);
+
+Console.WriteLine("");
+
+Console.Beep(1320, 200);
+Console.Write("Dó, ");
+Console.Beep(1485, 200);
+Console.Write("Ré, ");
+Console.Beep(1650, 200);
+Console.Write("Mi, ");
+Console.Beep(1759, 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Console.Write("Fá, ");
+Console.Beep(1759, 200);
+Console.Write("Fá. ");
+Thread.Sleep(500);
